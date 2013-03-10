79700301422baceed3dc092aaa8eda73fc65c091
claront@gmail.com <claront@gmail.com>
2013-03-09 22:33:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'ce0e6fa3c4348e2b2a79b84ece8baa032f4971d40ce9893e6645fb5db536ee9ad9e3e1f9e41883257bd4c438e0702c30830605e517bf4ef189a092b6af0063da'
